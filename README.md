bdd0ae0ed07cfc29916b7636312d5b49d75fec00
QuentiumYT <quentin.lienhardt@gmail.com>
2022-05-04 21:55:59 +0200

    Cleanup project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sync Scroll README
+# Sync Scroll
 
 A Visual Studio Code Extension that make split panels scroll synchronically.
 
@@ -6,76 +6,21 @@ A Visual Studio Code Extension that make split panels scroll synchronically.
 
 This extension support synchronizing scrolling between split panels. You can choose your sync mode to make them scrolled together.
 
-![Scroll synchronically when toggle on](./feature_mode.gif)
+![Scroll synchronically when toggle on](./resources/feature_mode.gif)
 
 There're also commands you can use conveniently.
 
-![Scroll synchronically when toggle on](./screenshot-command.png)
+![Scroll synchronically when toggle on](./resources/screenshot-command.png)
 
 Also it will automatically highlight the corresponding selections when you focus the cursor on the one side.
 
-![Corresponding selections highlighted](./feature_highlight.gif)
+![Corresponding selections highlighted](./resources/feature_highlight.gif)
 
 Here are two commands you can find in the right-click menu in the content window.
 
-![Scroll synchronically when toggle on](./screenshot-right-click-menu.png)
+![Scroll synchronically when toggle on](./resources/screenshot-right-click-menu.png)
 
 - `Jump to Next Panel Corresponding Position` for navigating around the panels and in the corresponding position. It's very convenient to quick focus on the other side.
 - `Copy to All Corresponding Places` for replacing all the text at corresponding positions from the selections. For example, it's for the case that you want to use the selected text on the left side also on the right side.
 
-## Release Notes
-
-### 1.3.0
-
-Add features:
-
-- Add command to jump to corresponding position in the next panel
-- Add command to copy selections to all corresponding positions.
-
-Enhancement:
-
-- Fix the issue of the output panel which shouldn't be involved in the scrolling sync.
-
-### 1.2.0
-
-Add features:
-
-- Add corresponding line highlight feature.
-
-Enhancement:
-
-- Fix back and forth scroll issue in diff(selecting file to compare)/scm(viewing file changes) case.
-
-### 1.1.1
-
-Enhancement:
-
-- Persist the toggle state and mode
-- Fix back and forth scroll issue in diff(selecting file to compare)/scm(viewing file changes) case.
-
-### 1.1.0
-
-Add features:
-
-- Now you can choose a sync mode when it turns on:
-  - NORMAL - aligned by the top of the view range.
-  - OFFSET - aligned by the scrolled lines offset.
-
-Enhancement:
-
-- Get rid of the scrolling delay.
-- Fix the issue that cannot toggle on/off when not focus on any editor.
-  
-### 1.0.0
-
-Initial release of Sync Scroll with features:
-
-* Can set all the split panels into scroll synchronized mode.
-
------------------------------------------------------------------------------------------------------------
-
-## How to Contribute
-
-This extension is created by VSCode Extension Template (TypeScript) by [Yeoman](https://vscode.readthedocs.io/en/latest/extensions/yocode/).
-
-Basically, you can work with this extension source code as a normal typescript project.
+#### I forked this repository because I needed to change some values. As I linted all the files and did my own drastic changes, I didn't planned to create a PR to not mess with the original project. Thanks dqisme for this extension!
--- src/states.ts
+++ src/states.ts
@@ -1,109 +1,118 @@
-import * as vscode from 'vscode'
-const [toggleCommand, changeModeCommand] = require('../package.json').contributes.commands
+import * as vscode from "vscode";
+const [toggleCommand, changeModeCommand] = require("../package.json").contributes.commands;
 
 enum ON_OFF {
-    ON = 'ON',
-    OFF = 'OFF',
+  ON = "ON",
+  OFF = "OFF",
 }
+
 enum MODE {
-    NORMAL = 'NORMAL',
-    OFFSET = 'OFFSET',
-} 
+  NORMAL = "NORMAL",
+  OFFSET = "OFFSET",
+}
 
 interface ModeMenuOption {
-    label: MODE
-    description: string
+  label: MODE;
+  description: string;
 }
 
 abstract class State<T = any> {
-    private vscodeCommand: vscode.Command
-    protected abstract key: string
-    protected abstract defaultValue: T
-    protected abstract executeCommand(callback: () => void): void
-    protected abstract toText(value: T): string
-    protected context: vscode.ExtensionContext
-    protected constructor(context: vscode.ExtensionContext, statusBarItemPriority: number, vscodeCommand: vscode.Command) {
-        this.context = context
-        this.vscodeCommand = vscodeCommand
-        this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, statusBarItemPriority)
-        this.statusBarItem.command = vscodeCommand.command
-        this.statusBarItem.tooltip = vscodeCommand.title
-        AllStates.states.push(this)
+  private vscodeCommand: vscode.Command;
+  protected abstract key: string;
+  protected abstract defaultValue: T;
+  protected abstract executeCommand(callback: () => void): void;
+  protected abstract toText(value: T): string;
+  protected context: vscode.ExtensionContext;
+  protected constructor(context: vscode.ExtensionContext, statusBarItemPriority: number, vscodeCommand: vscode.Command) {
+    this.context = context;
+    this.vscodeCommand = vscodeCommand;
+    this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, statusBarItemPriority);
+    this.statusBarItem.command = vscodeCommand.command;
+    this.statusBarItem.tooltip = vscodeCommand.title;
+    AllStates.states.push(this);
+  }
+  protected get(): T | undefined {
+    return this.context.workspaceState.get(this.key);
+  }
+  protected set(value: T | undefined) {
+    if (value === undefined) {
+      return;
     }
-    protected get(): T | undefined {
-        return this.context.workspaceState.get(this.key)
-    }
-    protected set(value: T | undefined) {
-        if (value === undefined) return
-        this.context.workspaceState.update(this.key, value)
-        this.statusBarItem.text = this.toText(value)
-    }
-    public statusBarItem: vscode.StatusBarItem
-    public init = () => this.set(this.get() ?? this.defaultValue)
-    public registerCommand = (callback: () => void = () => {}) =>
-        vscode.commands.registerCommand(this.vscodeCommand.command, () => {
-            this.executeCommand.call(this, callback)
-        })
+    this.context.workspaceState.update(this.key, value);
+    this.statusBarItem.text = this.toText(value);
+  }
+  public statusBarItem: vscode.StatusBarItem;
+  public init = () => this.set(this.get() ?? this.defaultValue);
+  public registerCommand = (callback: () => void = () => {}) =>
+    vscode.commands.registerCommand(this.vscodeCommand.command, () => {
+      this.executeCommand.call(this, callback);
+    });
 }
 
 export class OnOffState extends State<ON_OFF> {
-    protected defaultValue = ON_OFF.ON
-    protected key = 'syncScroll.onOff'
-    protected toText = (value: string) => `Sync Scroll: ${value}`
-    protected executeCommand(callback: () => void) {
-        this.set(this.isOff() ? ON_OFF.ON : ON_OFF.OFF)
-        callback()
-    }
-    public constructor(context: vscode.ExtensionContext) {
-        super(context, 201, toggleCommand)
-    }
-    public isOff = () => this.get() === ON_OFF.OFF
+  protected defaultValue = ON_OFF.ON;
+  protected key = "syncScroll.onOff";
+  protected toText = (value: string) => `Sync Scroll: ${value}`;
+  protected executeCommand(callback: () => void) {
+    this.set(this.isOff() ? ON_OFF.ON : ON_OFF.OFF);
+    callback();
+  }
+  public constructor(context: vscode.ExtensionContext) {
+    super(context, 201, toggleCommand);
+  }
+  public isOff = () => this.get() === ON_OFF.OFF;
 }
 
 export class ModeState extends State<MODE> {
-    protected key = 'syncScroll.mode'
-    protected defaultValue = MODE.NORMAL
-    protected toText = (value: string) => `Sync Scroll Mode: ${value}`
-    protected executeCommand(callback: () => void) {
-        vscode.window.showQuickPick<ModeMenuOption>(
-            [{
-                label: MODE.NORMAL,
-                description: 'aligned by the top of the view range',
-            },{
-                label: MODE.OFFSET,
-                description: 'aligned by the scrolled lines offset',
-            }],
-            { placeHolder: 'Select sync scroll mode' },
-        ).then(selectedOption => {
-            this.set(selectedOption?.label)
-        }).then(() => {
-            callback()
-        })
-    }
-    public constructor(context: vscode.ExtensionContext) {
-        super(context, 200, changeModeCommand)
-    }
-    public isOffsetMode = () => this.get() === MODE.OFFSET
+  protected key = "syncScroll.mode";
+  protected defaultValue = MODE.NORMAL;
+  protected toText = (value: string) => `Sync Scroll Mode: ${value}`;
+  protected executeCommand(callback: () => void) {
+    vscode.window
+      .showQuickPick<ModeMenuOption>(
+        [
+          {
+            label: MODE.NORMAL,
+            description: "aligned by the top of the view range",
+          },
+          {
+            label: MODE.OFFSET,
+            description: "aligned by the scrolled lines offset",
+          },
+        ],
+        { placeHolder: "Select sync scroll mode" }
+      )
+      .then((selectedOption) => {
+        this.set(selectedOption?.label);
+      })
+      .then(() => {
+        callback();
+      });
+  }
+  public constructor(context: vscode.ExtensionContext) {
+    super(context, 200, changeModeCommand);
+  }
+  public isOffsetMode = () => this.get() === MODE.OFFSET;
 }
 
 export class AllStates {
-    private static visibility: boolean
-    public static states: State[] = []
-    public static get areVisible() : boolean {
-        return AllStates.visibility
-    }
-    public static set areVisible(visibility: boolean) {
-        AllStates.states.forEach((state) => {
-            if (visibility) {
-                state.statusBarItem.show()
-            } else {
-                state.statusBarItem.hide()
-            }
-        })
-        AllStates.visibility = visibility
-    }
-    public static init(visibility: boolean) {
-        AllStates.states.forEach(state => state.init())
-        AllStates.areVisible = visibility
-    }
+  private static visibility: boolean;
+  public static states: State[] = [];
+  public static get areVisible(): boolean {
+    return AllStates.visibility;
+  }
+  public static set areVisible(visibility: boolean) {
+    AllStates.states.forEach((state) => {
+      if (visibility) {
+        state.statusBarItem.show();
+      } else {
+        state.statusBarItem.hide();
+      }
+    });
+    AllStates.visibility = visibility;
+  }
+  public static init(visibility: boolean) {
+    AllStates.states.forEach((state) => state.init());
+    AllStates.areVisible = visibility;
+  }
 }
